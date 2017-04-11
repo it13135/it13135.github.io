d8076cfec924b7086340d1cebb9d81b0f11efda6
it13135 <it13135@uom.edu.gr>
2017-04-11 22:06:19 +0300

    move

--- js/puzzle-dg.js
+++ js/puzzle-dg.js
@@ -36,6 +36,7 @@ $.fn.extend({
 
 function Move(e, t) {
     var n = false;
+    var no = $("#selection").val();
 	var EmptySquare = no*no;
     var r = $("#board").children("div:nth-child(" + EmptySquare + ")").css("left");
     var i = $("#board").children("div:nth-child(" + EmptySquare + ")").css("top");
