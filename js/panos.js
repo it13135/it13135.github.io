939a09224c5ef7dab7078b35e8a6693bd382800c
it13135 <it13135@uom.edu.gr>
2017-04-13 23:30:04 +0300

    oo

---
+++ js/panos.js
@@ -0,0 +1,44 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext('2d');
+window.onload = function() {
+context.fillStyle = 'red';
+var imageObj = new Image();
+var images = [];
+//imageObj.onload = function() {
+//  context.drawImage(imageObj,0,0,60,60,0,0,60,60);
+//};
+imageObj.src = 'images/grid4x4.png';
+var gm = new game(4,4, 60);
+gm.draw();
+}	
+function game(xtiles, ytiles, size){
+	this.xtiles = xtiles;
+	this.ytiles = ytiles;
+	this.size = size;
+	this.tiles = [];
+	for( var i=1; i<=xtiles; i++)  //rows
+		for( var j=1; i<=ytiles; i++)  //columns
+			tiles.push(new tile(i,j));
+	this.draw = DrawGame;
+}
+function tile(x,y){
+	this.xreal=x;
+	this.yreal=y;
+	this.x=x;
+	this.y=y;
+	this.move=Move();
+}
+
+function Move(){
+	alert("move");
+}
+
+function DrawGame(){
+	$.each(game.tiles, function(i, item){
+		var xfrom = (j-1)*60;
+		var xto = x + this.size;
+		var yfrom = (i-1)*60;
+		var yto = y + this.size;
+		context.drawImage(imageObj, xfrom, yfrom, xto, yto, xfrom, yfrom, xto, yto);
+	});
+}
